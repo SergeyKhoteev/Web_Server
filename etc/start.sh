1b75bebc05640712681694e472cd18adbf27f0f6
SergeyKhoteev <khoteev.s@gmail.com>
2022-02-10 11:54:49 +0300

    fixed gu config

--- etc/start.sh
+++ etc/start.sh
@@ -4,8 +4,14 @@
 
 if [[ `pwd` != '/home/box/web/etc' ]]
 then
-	cd /home/box
-	cp -r /home/box/web /home/box/Web_Server/
+	cp -r /home/box/Web_Server /home/box/web
 fi 
 
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
+sudo /etc/init.d/nginx start
+
+sudo ln -s /home/box/web/etc/gunicorn/gunicorn.service /etc/systemd/system/gunicorn.service
+sudo ln -s /home/box/web/etc/gunicorn/gunicorn.socket /etc/systemd/system/gunicorn.socket
+
+cd /home/box/web
+gunicorn -b 127.0.0.1:8080 -w=2 hello:app
