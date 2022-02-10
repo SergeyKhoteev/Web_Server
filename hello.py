1987c23f58fd233d1398cf26b9270ed523e1db3a
SergeyKhoteev <khoteev.s@gmail.com>
2022-02-10 16:45:52 +0300

    hello modified. Only string return

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 
 def app(environ, start_response):
     """Simplest possible application object"""
-    data = b'Hello, World!\n'
+    data = b'Hello, World!\n1231231231\n'
     status = '200 OK'
     # resp = {}
     len_msg = 0
@@ -10,12 +10,19 @@ def app(environ, start_response):
         resp_1.append(((str(key) + ': ') + (str(environ.get(key)) + '\n')).encode('utf-8'))
         len_msg += len(str(key)) + len(str(environ.get(key))) + 5
     qs = (environ.get('QUERY_STRING', None)).split('&')
+    print(qs)
     for i in range(len(qs)):
-        qs[i] = (str(qs[i]) + '\n').encode('utf-8')
+        print(qs[i])
+        qs[i] = (str(qs[i]) + '\n')
         len_msg += len(qs[i])
+    qs_n = (''.join(qs))
+    print(qs_n)
+    print(type(qs_n))
+
+    qs_n = qs_n.encode('utf-8')
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len_msg))
     ]
     start_response(status, response_headers)
-    return iter(qs)
+    return iter([qs_n])
