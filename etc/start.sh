5a4f3d0a03e1d7554592ff1b229577be87f748f7
SergeyKhoteev <khoteev.s@gmail.com>
2022-04-07 16:19:16 +0300

    settings fixed + start.sh

--- etc/start.sh
+++ etc/start.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
+!/bin/bash
 
 
 
-# if [[ `pwd` != '/home/box/web/etc' ]]
-# then
-# 	cp -r /home/box/Web_Server /home/box/web
-# fi 
+if [[ `pwd` != '/home/box/web/etc' ]]
+then
+	cp -r /home/box/Web_Server /home/box/web
+fi 
 
-# ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
+ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 
-# sudo /etc/init.d/nginx start
+sudo /etc/init.d/nginx start
 
-# sudo /etc/init.d/mysql start
+sudo /etc/init.d/mysql start
 
 sudo mysql -u root -e "CREATE DATABASE boxask1;"
 sudo mysql -u root -e "CREATE USER 'askbox1'@'localhost' IDENTIFIED BY 'ord';"
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'askbox1'@'localhost' WITH GRANT OPTION;"
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON \`boxask1\`.* TO 'askbox1'@'localhost';"
  
-# sudo ln -s /home/box/web/etc/gunicorn/gunicorn.service /etc/systemd/system/gunicorn.service
-# sudo ln -s /home/box/web/etc/gunicorn/gunicorn.socket /etc/systemd/system/gunicorn.socket
+sudo ln -s /home/box/web/etc/gunicorn/gunicorn.service /etc/systemd/system/gunicorn.service
+sudo ln -s /home/box/web/etc/gunicorn/gunicorn.socket /etc/systemd/system/gunicorn.socket
 
-# cd /home/box/web
+cd /home/box/web
 
-# gunicorn -c gunicorn.hello.conf.py hello:app &
-# gunicorn -c gunicorn.django.conf.py ask.wsgi:application &
+gunicorn -c gunicorn.hello.conf.py hello:app &
+gunicorn -c gunicorn.django.conf.py ask.wsgi:application &
